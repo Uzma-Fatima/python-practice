4b57f6dc1553b0f3b599a0c2cc49ba8d5152519d
Uzma-Fatima <uzma.fatima0111@gmail.com>
2025-12-27 01:13:12 +0530

    Add files via upload

---
+++ day1_hello.py
@@ -0,0 +1,36 @@
+print("Hello, Python")
+print("My name is Uzma")
+print("Iam learning Python step by step")
+print("I will become confident in coding")
+name = "Uzma"
+age = 18
+print(name)
+print(age)
+city = "Hyderabad"
+college = "Lords"
+print(city)
+print(college)
+age = 19
+print(age)
+name = input("Enter your name: ")
+print(name)
+age = input("Enter your age: ")
+print(age)
+name = input("Enter your name: ")
+city = input("Enter your city: ")
+print(name)
+print(city)
+print("Revision done")
+print("Hello")
+print("How are you?")
+print("Iam learning Python")
+#This line will not run
+print("Comments are ignored by Python")
+name = "Uzma"
+age = 20
+print(name)
+print(age)
+city = "Hyderabad"
+course = "Python"
+print(city)
+print(course)
